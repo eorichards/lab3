92a0193eada0effcf1d02fe9c0f3cd814aa54840
eorichar@ucsd.edu <ericrichards@Erics-MacBook-Pro.local>
2018-01-25 10:04:51 -0800

    Modified Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,26 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
-
+	function projectClick(e) { 
+	$('.jumbotron h1').text("Javascript is connected");
+    // prevent the page from reloading      
+    e.preventDefault();
+    $(".jumbotron h1").text("Javascript has taken control");
+    $(".jumbotron p").toggleClass("active");
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    // $(this).css("background-color", "#7fff00");
+    var containingProject = $(".project#project1");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+}
+	$("#testjs").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	// $("a.thumbnail").click(projectClick);
 }
+
